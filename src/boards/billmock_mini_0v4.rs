1a0ddf1e6b2d6b72edeba05bb4fe0144cb087a15
pmnxis <pmnxis@gmail.com>
2023-09-18 01:06:41 +0900

    hardware 0v4 and mini-0v4 basic adoption

--- src/boards/billmock_mini_0v4.rs
+++ src/boards/billmock_mini_0v4.rs
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: MIT OR Apache-2.0
  */
 
-//! Hardware initialization code for BillMock Hardware Version 0.4
-//! The code follows on version 0.4 schematic
-//! https://github.com/pmnxis/BillMock-HW-RELEASE/blob/master/sch/BillMock-HW-0v4.pdf
+//! Hardware initialization code for BillMock Hardware Version mini 0.4
+//! The code follows on version mini 0.4 schematic
+//! https://github.com/pmnxis/BillMock-HW-RELEASE/blob/master/sch/BillMock-Mini-HW-0v4.pdf
 
 use embassy_stm32::exti::{Channel as HwChannel, ExtiInput};
 use embassy_stm32::gpio::{Input, Level, Output, Pin, Pull, Speed};
@@ -32,7 +32,7 @@ bind_interrupts!(struct Irqs {
 static mut USART2_RX_BUF: [u8; components::serial_device::CARD_READER_RX_BUFFER_SIZE] =
     [0u8; components::serial_device::CARD_READER_RX_BUFFER_SIZE];
 
-pub fn hardware_init_0v4(
+pub fn hardware_init_mini_0v4(
     p: embassy_stm32::Peripherals,
     shared_resource: &'static SharedResource,
 ) -> Hardware {
@@ -68,12 +68,12 @@ pub fn hardware_init_0v4(
                 Player::Player1,
                 Output::new(p.PA1.degrade(), Level::Low, Speed::Low), // REAL0_INH
                 ExtiInput::new(
-                    Input::new(p.PB2, Pull::None).degrade(), // REAL0_VND
-                    p.EXTI2.degrade(),                       // EXTI2
+                    Input::new(p.PB14, Pull::None).degrade(), // REAL0_VND
+                    p.EXTI14.degrade(),                       // EXTI14
                 ),
                 ExtiInput::new(
-                    Input::new(p.PB14, Pull::None).degrade(), // REAL0_STR
-                    p.EXTI14.degrade(),                       // EXTI14
+                    Input::new(p.PB2, Pull::None).degrade(), // REAL0_STR
+                    p.EXTI2.degrade(),                       // EXTI2
                 ),
                 async_input_event_ch,
                 &shared_resource.arcade_players_timing[PLAYER_1_INDEX],
@@ -82,12 +82,12 @@ pub fn hardware_init_0v4(
                 Player::Player2,
                 Output::new(p.PA0.degrade(), Level::Low, Speed::Low), // REAL1_INH
                 ExtiInput::new(
-                    Input::new(p.PB11, Pull::None).degrade(), // REAL1_VND
-                    p.EXTI11.degrade(),                       // EXTI11
+                    Input::new(p.PD1, Pull::None).degrade(), // REAL1_VND
+                    p.EXTI1.degrade(),                       // EXTI1
                 ),
                 ExtiInput::new(
-                    Input::new(p.PD1, Pull::None).degrade(), // REAL1_STR
-                    p.EXTI1.degrade(),                       // EXTI1
+                    Input::new(p.PB11, Pull::None).degrade(), // REAL1_STR
+                    p.EXTI11.degrade(),                       // EXTI11
                 ),
                 async_input_event_ch,
                 &shared_resource.arcade_players_timing[PLAYER_2_INDEX],
@@ -113,22 +113,22 @@ pub fn hardware_init_0v4(
                     Input::new(p.PA15, Pull::None).degrade(), // VIRT1_INH
                     p.EXTI15.degrade(),                       // EXTI15
                 ),
-                Output::new(p.PC13.degrade(), Level::Low, Speed::Low), // VIRT1_BSY
-                Output::new(p.PB5.degrade(), Level::Low, Speed::Low),  // VIRT1_VND
-                Output::new(p.PC15.degrade(), Level::Low, Speed::Low), // VIRT1_JAM
-                Output::new(p.PC14.degrade(), Level::Low, Speed::Low), // VIRT1_STR
+                Output::new(p.PC14.degrade(), Level::Low, Speed::Low), // VIRT1_BSY
+                Output::new(p.PC13.degrade(), Level::Low, Speed::Low), // VIRT1_VND
+                Output::new(p.PB5.degrade(), Level::Low, Speed::Low),  // VIRT1_JAM
+                Output::new(p.PC15.degrade(), Level::Low, Speed::Low), // VIRT1_STR
                 async_input_event_ch,
                 &shared_resource.arcade_players_timing[PLAYER_2_INDEX],
             ),
         ],
         indicators: [
             BufferedOpenDrain::new(
-                Output::new(p.PA4.degrade(), Level::High, Speed::Low),
+                Output::new(p.PA5.degrade(), Level::High, Speed::Low),
                 &shared_resource.indicator_timing,
                 BufferedOpenDrainKind::Indicator(1).const_str(),
             ),
             BufferedOpenDrain::new(
-                Output::new(p.PA5.degrade(), Level::High, Speed::Low),
+                Output::new(p.PA4.degrade(), Level::High, Speed::Low),
                 &shared_resource.indicator_timing,
                 BufferedOpenDrainKind::Indicator(2).const_str(),
             ),
