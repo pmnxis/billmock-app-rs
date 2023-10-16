e452598545432d0ad7fab853234bb4f110ccfae4
pmnxis <pmnxis@gmail.com>
2023-10-17 02:58:20 +0900

    example billmock-plug-card need avoiding unimplement! error

--- billmock-plug-card/src/lib.rs
+++ billmock-plug-card/src/lib.rs
@@ -14,7 +14,6 @@ mod common;
 
 use card_terminal_adapter::types::*;
 use card_terminal_adapter::*;
-use common::*;
 
 // #[cfg(any(build, test))]
 // mod helper;
@@ -28,105 +27,119 @@ impl CardTerminalConst for KiccEd785Plug {
 }
 
 impl CardTerminalRxParse for KiccEd785Plug {
-    fn pre_parse_common(&self, raw: &[u8]) -> Result<CardTerminalRxCmd, CardTerminalError> {
-        unimplemented!()
+    fn pre_parse_common(&self, _raw: &[u8]) -> Result<CardTerminalRxCmd, CardTerminalError> {
+        // implement me for actual usage
+        Err(CardTerminalError::UnsupportedSpec)
     }
 
     fn post_parse_response_sale_slot_info(
         &self,
-        raw: &[u8],
+        _raw: &[u8],
     ) -> Result<CardReaderPortBackup, CardTerminalError> {
-        unimplemented!()
+        // implement me for actual usage
+        Err(CardTerminalError::UnsupportedSpec)
     }
 
     fn post_parse_response_terminal_info(
         &self,
-        raw: &[u8],
+        _raw: &[u8],
     ) -> Result<(TerminalVersion, RawTerminalId), CardTerminalError> {
-        unimplemented!()
+        // implement me for actual usage
+        Err(CardTerminalError::UnsupportedSpec)
     }
 }
 
 impl CardTerminalTxGen for KiccEd785Plug {
     fn response_ack<'a>(&self, _buffer: &'a mut [u8]) -> &'a [u8] {
+        // KICC common ACK spec
         &common::RAW_DATA_ACK
     }
 
     fn response_nack<'a>(&self, _buffer: &'a mut [u8]) -> &'a [u8] {
+        // KICC common NACK spec
         &common::RAW_DATA_NACK
     }
 
     fn response_device_info<'a>(
         &self,
         buffer: &'a mut [u8],
-        model_version: &'a [u8; FW_VER_LEN],
-        serial_number: &'a [u8; DEV_SN_LEN],
+        _model_version: &'a [u8; FW_VER_LEN],
+        _serial_number: &'a [u8; DEV_SN_LEN],
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn alert_coin_paper_acceptor_income<'a>(
         &self,
         buffer: &'a mut [u8],
-        income: RawU24IncomeArcade,
+        _income: RawU24IncomeArcade,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn push_sale_slot_info<'a>(
         &self,
         buffer: &'a mut [u8],
-        port_backup: &'a CardReaderPortBackup,
+        _port_backup: &'a CardReaderPortBackup,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn push_sale_slot_info_partial_inhibit<'a>(
         &self,
         buffer: &'a mut [u8],
-        port_backup: &'a CardReaderPortBackup,
+        _port_backup: &'a CardReaderPortBackup,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
-    fn request_sale_slot_info<'a>(&self, _buffer: &'a mut [u8]) -> &'a [u8] {
-        unimplemented!()
+    fn request_sale_slot_info<'a>(&self, buffer: &'a mut [u8]) -> &'a [u8] {
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
-    fn request_terminal_info<'a>(&self, _buffer: &'a mut [u8]) -> &'a [u8] {
-        unimplemented!()
+    fn request_terminal_info<'a>(&self, buffer: &'a mut [u8]) -> &'a [u8] {
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn display_rom<'a>(
         &self,
         buffer: &'a mut [u8],
-        git_hash: &'a [u8; GIT_HASH_LEN],
-        terminal_id: &'a [u8; TID_LEN],
-        p1_card: u32,
-        p2_card: u32,
-        p1_coin: u32,
-        p2_coin: u32,
+        _git_hash: &'a [u8; GIT_HASH_LEN],
+        _terminal_id: &'a [u8; TID_LEN],
+        _p1_card: u32,
+        _p2_card: u32,
+        _p1_coin: u32,
+        _p2_coin: u32,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn display_hw_info<'a>(
         &self,
         buffer: &'a mut [u8],
-        model_version: &'a [u8; FW_VER_LEN],
-        serial_number: &'a [u8; DEV_SN_LEN],
-        terminal_id: &'a [u8; TID_LEN],
-        hw_boot_cnt: u32,
-        uptime_minutes: u32,
+        _model_version: &'a [u8; FW_VER_LEN],
+        _serial_number: &'a [u8; DEV_SN_LEN],
+        _terminal_id: &'a [u8; TID_LEN],
+        _hw_boot_cnt: u32,
+        _uptime_minutes: u32,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 
     fn display_warning<'a>(
         &self,
-        _buffer: &'a mut [u8],
-        warn_kind: CardTerminalDisplayWarning,
+        buffer: &'a mut [u8],
+        _warn_kind: CardTerminalDisplayWarning,
     ) -> &'a [u8] {
-        unimplemented!()
+        // implement me for actual usage
+        &buffer[0..0]
     }
 }
